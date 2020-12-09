04d20868d77a061a1064ede56fbe255ca11d8357
Serkara2de <53303465+Serkara2de@users.noreply.github.com>
2020-12-09 16:03:40 +0100

    First Day

--- src/test/java/com/cybertek/test/day1_intro/OpenBrowser.java
+++ src/test/java/com/cybertek/test/day1_intro/OpenBrowser.java
@@ -4,15 +4,18 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class OpenBrowser {
 
     public static void main(String[] args) {
 
         WebDriverManager.chromedriver().setup();
 
+
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://cybertekschool.com");
+
+        driver.get("https://amazon.com");
 
 
 
