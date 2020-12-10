286ad232c13d4aabaa9699580b62f02304aaadb8
Serkara2de <53303465+Serkara2de@users.noreply.github.com>
2020-12-10 01:35:36 +0100

    Second Day

---
+++ src/test/java/com/cybertek/tests/day2_webdriver_basics/NavigationDemo.java
@@ -0,0 +1,37 @@
+package com.cybertek.tests.day2_webdriver_basics;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigationDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        Thread.sleep(2000);
+
+        //navigate to another website with different selenium method
+        driver.navigate().to("https://facebook.com");
+
+        //wait 3 seconds here and then move on
+        Thread.sleep(3000);
+
+        //goes back to previus page
+        driver.navigate().back();
+
+        Thread.sleep(2000);
+
+        //goes forward to next page
+        driver.navigate().forward();
+
+        //refresh to webpage
+        driver.navigate().refresh();
+
+    }
+}
