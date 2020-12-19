c445461ce1b0739536c7a0f70d83bf8c63e43e63
Serkara2de <53303465+Serkara2de@users.noreply.github.com>
2020-12-19 01:18:30 +0100

    Day 2 Web Driver Basics

--- src/test/java/com/cybertek/tests/day2_webdriver_basics/CallWebDriverFactory.java
+++ src/test/java/com/cybertek/tests/day2_webdriver_basics/CallWebDriverFactory.java
@@ -8,6 +8,7 @@ public class CallWebDriverFactory {
     public static void main(String[] args) {
 
        WebDriver driver = WebDriverFactory.getDriver("firefox");
+       driver.manage().window().maximize();
 
        driver.get("https://www.google.com");
 
