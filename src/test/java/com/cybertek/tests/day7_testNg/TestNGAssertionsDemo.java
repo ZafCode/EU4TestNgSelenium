7950a5d41b7dccbd9caf890a1cce0be55c4ee5cc
Serkara2de <53303465+Serkara2de@users.noreply.github.com>
2020-12-24 00:34:12 +0100

    Day 7 TestNg

--- src/test/java/com/cybertek/tests/day7_testNg/TestNGAssertionsDemo.java
+++ src/test/java/com/cybertek/tests/day7_testNg/TestNGAssertionsDemo.java
@@ -33,6 +33,35 @@ public class TestNGAssertionsDemo {
         Assert.assertEquals("test2", "test2");
     }
 
+    @Test
+    public void test3(){
+        String expectedTitle = "Cyt";
+        String actualTitle = "Cybertek";
+
+        Assert.assertTrue(actualTitle.startsWith(expectedTitle), "Verify title starts with Cyb");
+
+    }
+
+
+    @Test
+    public void test4(){
+        // verify email contains @
+
+        String email = "mike@smith.com";
+
+        Assert.assertTrue(email.contains("@"), "verify email contains @");
+    }
+
+    @Test
+    public void test5(){
+        Assert.assertFalse(0>1, "verify that 0 is not greater than 1");
+    }
+
+    @Test
+    public void test6(){
+        Assert.assertNotEquals("one", "oNe", "if value first not equal to value second");
+    }
+
     @AfterMethod
     public void tearDown(){
         System.out.println("Close browser");
