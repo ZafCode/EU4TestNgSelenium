569fbdff7336271e9dcc5698de445f8c2e55aa20
Serkara2de <53303465+Serkara2de@users.noreply.github.com>
2021-01-16 00:48:24 +0100

    Login Page Updates

--- src/test/java/com/cybertek/pages/LoginPage.java
+++ src/test/java/com/cybertek/pages/LoginPage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
-public class LoginPage extends TestBase{
+public class LoginPage {
 
     public LoginPage() {
 
